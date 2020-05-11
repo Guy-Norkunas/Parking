7bc0981eda75e2d631de1ceac7f730c21291e127
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-10 22:40:59 -0700

    map should now be centered around the users location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    visitor_ip = request.remote_ip
+    results = Geocoder.search(visitor_ip)
+    @user_location = results.first.coordinates
     @listings = Listing.all
   end
 end
