c831e2b25fa30d99555b90472e026245c6ba2e78
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-12 14:47:27 +1000

    should give user coordinates now?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 class HomeController < ApplicationController
   def index
     
-    @location = get_user_coordinates
+    @coordinates = get_user_coordinates
     @listings = Listing.all
   end
 
   private
 
   def get_user_coordinates
-    visitor_ip = request.remote_ip
-    results = Geocoder.search("2001:8003:418a:201:48c1:6e47:2d43:b498")
-    location = results.first.address
     city = request.location.city
-    return visitor_ip
+    results = Geocoder.search(city)
+    coordinates = results.first.coordinates
+    
+    return coordinates
   end
 end
